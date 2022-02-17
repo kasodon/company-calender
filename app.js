92aa067979c4851a913b2594e4cdf42393e18ed4
Edewor Onyedika <edwordkaso100@gmail.com>
2022-02-17 15:09:19 +0100

    furnished changes

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+ });
+
 router.post('/company', (req, res) => {
    // res.json(private)
    if (req.body.company_type === "public") {
